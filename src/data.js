0fb50d2b3ac963f0172b0efaf1f71ffe6d39c8f7
SFNTK <soufiane.fone@gmail.com>
2024-01-06 21:57:04 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,42 @@
+import images from './pictures/index'
+const data = [{
+   id: 1,
+   description: "UP Milan's 2023 jersey: Bold design, iconic colors. A blend of heritage and modernity, capturing Milan's spirit in a sleek, contemporary style for the true football aficionado."
+   , "name": "milan jersey 2022", price: "320", rating: 5, taille: "S",
+   "quantity": 33, image: images.milancaro, "verified": false
+}, {
+   id: 2,
+   description: "UP inter Milan's 2021 jersey: Bold design, iconic colors. A blend of heritage and modernity, capturing INTER Milan's spirit in a sleek, contemporary style for the true football aficionado.",
+   "name": "INTER milan jersey 2021",
+   price: "420", rating: 4, image: images.inter2022,
+   taille: "S", "quantity": 27, "verified": false
+}, {
+   id: 3,
+   description: "The REAL MADRID jersey from 2002 is iconic and holds a special place in the hearts of fans. It featured the classic all-white design, which is synonymous with the club's traditional colors. The jersey had a simple yet elegant look, with thin black accents on the sleeves and collar, giving it a sleek and stylish appearance. The club crest was proudly displayed on the left side of the chest, while the Adidas logo adorned the right. The 2002 jersey is particularly memorable as it was worn during a successful period for the club, including winning the UEFA Champions League that year, adding to its significance and popularity among fans and collectors alike.",
+   "name": "real madrid retro jersey 1998", price: "520", rating: 5, image: images.real2002
+   , taille: "S", "quantity": 88, "verified": true
+
+}, {
+   id: 4,
+   description: "The Morocco national football team's jersey in 1998 had a distinctive design. It was predominantly red with green accents. The jersey typically featured the Moroccan Football Federation's emblem on the left side of the chest, while the team's sponsor logos and possibly other details adorned the shirt.",
+   "name": "MAROC WORLD CUP jersey 2022 QATAR",
+   price: "520", rating: 5, image: images.imgmaroc,
+   taille: "S", "quantity": 31, "verified": true
+
+}, {
+   id: 5
+   , description: `The 2023 Napoli jersey is a stunning blend of tradition and modernity. Predominantly azure blue, it encapsulates the essence of the city and the passion of its fans. The jersey features subtle, intricate patterns reminiscent of Naples' rich cultural heritage, with delicate accents in gold and white that pay homage to the club's prestigious history.
+
+   The shirt boasts a sleek V-neck collar, providing a classic yet contemporary look. The club crest sits proudly on the left side of the chest, embroidered in vivid detail, symbolizing the unity and strength of the team. Adorned with the sponsor's logo, the jersey maintains a harmonious balance between tradition and the necessities of modern football.
+   
+   The fabric is lightweight and breathable, designed for optimal performance on the pitch while ensuring comfort for the players. Advanced technology incorporated into the material helps regulate body temperature, wicking away moisture to keep the players cool and dry during intense matches.
+   
+   The back of the jersey displays the players' names and numbers in a bold, elegant font, creating a striking contrast against the azure backdrop. Overall, the 2023 Napoli jersey is a visually captivating tribute to the club's heritage, embodying the pride and fervor of its supporters while embracing innovation in design and functionality.`,
+   "name": "napoli  jersey 2023",
+   price: "320", rating: 5
+   , image: images.napoli, taille: "S", "quantity": 3,
+   "verified": true
+
+}];
+
+export default data
